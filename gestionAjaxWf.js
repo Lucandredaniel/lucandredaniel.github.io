0d875f2d54de1bc6df62c701833c876aad88dbfa
Luc <143090734+Lucandredaniel@users.noreply.github.com>
2023-10-19 16:13:58 +0200

    Modifications V39-3

--- gestionAjaxWf.js
+++ gestionAjaxWf.js
@@ -2,13 +2,7 @@
 /* ===================================================== */
 
 function mise_en_forme_tableau(index){
-    tableau_en_forme=""
-    for (let i = 0; i < base_donnees_complete[index].length; i++) {
-        tableau_en_forme=tableau_en_forme+base_donnees_complete[index][i]+",";
-    }
-}
-
-function AAEfichier(){
 
 }
+
 
